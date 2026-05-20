bbd234352099bfe6c2a71f6869f1783e585b6b4f
Martin Espinosa <felipe.espinosa@udla.edu.ec>
2026-05-20 14:56:54 -0500

    funcion de promedio : miercoles

--- ejercicio.c
+++ ejercicio.c
@@ -1,15 +1,18 @@
-
 #include <stdio.h>
-
+float calcular_promedio(float notas[], int tamaño);
 int main() {
     float notas[]={6.5, 7.1, 8, 6, 5};
+    float promedio=calcular_promedio(notas,5);
+    printf("\nel promedio de las notas %.1f\n", promedio);
+    return 0;
+}
+float calcular_promedio(float notas[], int tamaño){
     float suma=0;
     float promedio=0;
-    for (int i=0; i<5; i++){
+    for (int i=0; i<tamaño; i++){
         printf("nota %d: %.1f\n",i+1, notas[i]);
         suma = suma+notas[i];
     }
-    promedio=suma/5;
-    printf("\nel promedio de las notas %.1f\n", promedio);
-    return 0;
+    promedio=suma/tamaño;
+    return promedio;
 }
